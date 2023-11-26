b08a1018d3167b76343f1234c2f55a67b30162a8
Roberto Bocio Melo <robertobocio@01lab.co>
2023-11-26 16:13:06 -0500

    Refactor userRouter export

--- users/src/user/user.router.ts
+++ users/src/user/user.router.ts
@@ -5,12 +5,10 @@ import { validation } from "../shared/controllers";
 import { userController } from "./user.dependencies";
 import { CreateUserDto, GetUsersDto, UpdateUserDto } from "./user.dto";
 
-const userRouter = Router();
+export const userRouter = Router();
 
 userRouter
   .get("/:id", validation(GetUsersDto), userController.findById.bind(userController))
   .post("/", validation(CreateUserDto), userController.create.bind(userController))
   .patch("/:id", validation(UpdateUserDto), userController.update.bind(userController))
   .delete("/:id", validation(GetUsersDto), userController.delete.bind(userController));
-
-export { userRouter };
